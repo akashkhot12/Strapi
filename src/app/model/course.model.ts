cf7a9afd0787dcf52cc6f070325a2a5309c1215d
akashkhot12 <akashkhot03@gmail.com>
2023-06-30 15:00:12 +0530

    course module to get data

---
+++ src/app/model/course.model.ts
@@ -0,0 +1,5 @@
+import { Data } from "@angular/router";
+
+export interface course{
+    data ? : any[]
+  }
---
+++ src/app/model/data.model.ts
@@ -0,0 +1,7 @@
+import { attributes } from "./attribute.model";
+
+export interface Data{
+    id ? : number ; 
+    attribute ? : attributes[] ;  
+    
+}
